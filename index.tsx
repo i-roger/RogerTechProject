76e4ac75e971edb17105700715418324e0a384de
Matheus Roger <58693188+i-roger@users.noreply.github.com>
2023-06-23 18:53:17 -0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/navbar'
+export default function Home() {
+  return (
+    <>
+    <body>
+    <Navbar />
+    </body>
+    <main>
+    <div className='flex justify-center'>YEahsuidshaiudhiauhsd</div>
+    </main>
+    </>
+  )
+}
